5fde6ccb3dc753a7f5b5d00df7a005cc306cb2bd
zhou-fuyi <thread.zhou@gmail.com>
2023-05-18 23:00:31 +0800

    feat: 结构初始化

---
+++ src/main/java/org/fuyiatlas/map/micromapservice/MicroMapServiceApplication.java
@@ -0,0 +1,13 @@
+package org.fuyiatlas.map.micromapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroMapServiceApplication.class, args);
+	}
+
+}
